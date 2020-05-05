23c027cca220658b96ce2d95b790008090f3a7ec
noiz <63849061+NoizzioN@users.noreply.github.com>
2020-05-05 13:25:15 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+$(function(){
+    $('.top-title').fadeIn(3000);
+    $('.twitter').show(4000);
+    $('.instagram').show(4000);
+    $('.menu-icon').fadeIn(5000)
+
+    function toggleNav() {
+        var body = document.body;
+        var hamburger = document.getElementById('js-hamburger');
+        var blackBg = document.getElementById('js-black-bg');
+      
+        hamburger.addEventListener('click', function() {
+          body.classList.toggle('nav-open');
+        });
+        blackBg.addEventListener('click', function() {
+          body.classList.remove('nav-open');
+        });
+      }
+      toggleNav();
+    
+    
+});
